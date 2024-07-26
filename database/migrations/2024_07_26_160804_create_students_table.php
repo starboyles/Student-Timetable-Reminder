d8d22a58d6667bdb3d37a25de87f6a66252c0480
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-07-26 16:23:03 +0000

    fix

--- database/migrations/2024_07_26_160804_create_students_table.php
+++ database/migrations/2024_07_26_160804_create_students_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('gender');
             $table->string('email');
             $table->string('phone_number');
             $table->string('course_name');
